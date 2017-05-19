5ff429dc01f14c96ba51e539dd611dffa50412ce
Robert Springer <springerdrums@gmail.com>
2017-05-18 19:27:59 -0700

    adding stuff

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-console.log("suhhh")
+$(document).ready(function(){
+  console.log("hmmmm")
+  $('#location-btn').on("click", function(){
+    var location = $('#location-input').val();
+    if(location != ''){
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&APPID=8c051d7ffccf2ab96aef8e3b478267a2",
+        type: "get",
+        success: function(data){
+          console.log(data)
+          $('#temperature').html("Temperature: " + data.main.temp + " degrees farenheit")
+          $('#weather').html("Weather: " + data.weather[0].description)
+          $('#humidity').html("Humidity: " + data.main.humidity + "%")
+        }
+      })
+    }else{
+      alert('You must enter a location')
+    }
+  })
+  // $.ajax({
+  //   method: "GET",
+  //   url: "http://api.wunderground.com/api/3b7dd63fa1ba1bda/conditions/q/CA/San_Francisco.json",
+  //   success: function(data){
+  //     console.log(data)
+  //   }
+  // })
+
+})
